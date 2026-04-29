1012362e543b1f0820bbad67dd0f23afa37fd783
Utku Melemetci <55263178+utkudotdev@users.noreply.github.com>
2026-04-29 19:22:43 -0400

    sample start state

--- collect_dset.py
+++ collect_dset.py
@@ -6,6 +6,7 @@ import jax
 import jax.numpy as jnp
 import functools as ft
 
+
 def get_dubins_parameters() -> DubinsParameters:
     return DubinsParameters(
         turn_rate_min=jnp.array(-1.0),
@@ -13,14 +14,32 @@ def get_dubins_parameters() -> DubinsParameters:
         velocity_min=jnp.array(-1.0),
         velocity_max=jnp.array(1.0),
         acceleration_min=jnp.array(-2.0),
-        acceleration_max=jnp.array(2.0)
+        acceleration_max=jnp.array(2.0),
     )
+
 
 def get_obstacle_parameters() -> ObstacleParameters:
     return from_many(
-        ObstacleParameters(radius=jnp.array(1.5), speed=jnp.array(0.0), start_point=jnp.array([4.0, 4.0]), end_point=jnp.array([0.0, 0.0])), 
-        ObstacleParameters(radius=jnp.array(0.5), speed=jnp.array(0.6), start_point=jnp.array([5.7, 4.0]), end_point=jnp.array([5.7, 2.25])),
-        ObstacleParameters(radius=jnp.array(0.5), speed=jnp.array(0.0), start_point=jnp.array([5.7, 4.0]), end_point=jnp.array([5.7, 0.0])))
+        ObstacleParameters(
+            radius=jnp.array(1.5),
+            speed=jnp.array(0.0),
+            start_point=jnp.array([4.0, 4.0]),
+            end_point=jnp.array([0.0, 0.0]),
+        ),
+        ObstacleParameters(
+            radius=jnp.array(0.5),
+            speed=jnp.array(0.6),
+            start_point=jnp.array([5.7, 4.0]),
+            end_point=jnp.array([5.7, 2.25]),
+        ),
+        ObstacleParameters(
+            radius=jnp.array(0.5),
+            speed=jnp.array(0.0),
+            start_point=jnp.array([5.7, 4.0]),
+            end_point=jnp.array([5.7, 0.0]),
+        ),
+    )
+
 
 def get_parameters() -> Parameters:
     x_min, x_max = 0.0, 8.0
@@ -34,23 +53,37 @@ def get_parameters() -> Parameters:
         y_max=jnp.array(y_max),
     )
 
+
 def sample_start_state(key: jnp.ndarray, p: Parameters) -> State:
-    keys = jax.random.split(key, 20)
+    N_OBSTACLES = len(p.obstacle_params.start_point)
+    obstacles_key, x_key, y_key, v_key, theta_key = jax.random.split(key, 20)
+
+    obstacle_states = []
+    for key in jax.random.split(obstacles_key, N_OBSTACLES):
+        alpha_key, forward_key = jax.random.split(key)
+        state = ObstacleState(
+            alpha=jax.random.uniform(alpha_key, minval=0, maxval=1.0),
+            forward=jax.random.uniform(forward_key) > 0.5,
+        )
+        obstacle_states.append(state)
 
     return State(
         dubins_state=DubinsState(
-            theta=jax.random.uniform(keys[3], minval=0, maxval=2 * jnp.pi),
+            x=jax.random.uniform(x_key, minval=p.x_min, maxval=p.x_max),
+            y=jax.random.uniform(y_key, minval=p.y_min, maxval=p.y_max),
+            v=jax.random.uniform(
+                v_key,
+                minval=p.dubins_params.velocity_min,
+                maxval=p.dubins_params.velocity_max,
+            ),
+            theta=jax.random.uniform(theta_key, minval=0, maxval=2 * jnp.pi),
         ),
-        obstacle_state=from_many(
-            ObstacleState( 
-                alpha=jax.random.uniform(keys[1], minval=0, maxval=1.0),
-                forward=jax.random.uniform(keys[2]) >= 0.5
-            )
-        )
+        obstacle_state=from_many(obstacle_states),
     )
 
 
 NUM_SAMPLES = 1024
+
 
 def main():
     triples = []
@@ -63,9 +96,7 @@ def main():
         key, sample_key = jax.random.split(key)
         start_state = sample_start_state(sample_key)
 
-        
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
+
